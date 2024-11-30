3b9871481a5f289915473be00a100278b4b9e389
skulidropek <66840575+skulidropek@users.noreply.github.com>
2024-11-30 05:05:09 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/LibreExpert/', // Укажите имя репозитория
   plugins: [react()],
   resolve: {
     alias: {
